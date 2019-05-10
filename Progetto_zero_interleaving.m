19c9c58541391eaf58b0065c6db5bf3c8459c7de
A4AndreCRC <37935364+A4AndreCRC@users.noreply.github.com>
2019-05-11 00:58:46 +0200

    Update Progetto_zero_interleaving.m

--- Progetto_zero_interleaving.m
+++ Progetto_zero_interleaving.m
@@ -1,3 +1,4 @@
+CONSEGNA
 % PROGETTO ZERO INTERLEAVING
 %
 % Lo script deve ricostruire una sequenza x_n a partire da una sua versione "zero interleaved" indicata con y_n 
@@ -34,42 +35,81 @@
 % diverse da quella assegnata (es. diverso numero di campioni).
 % Definire in modo chiaro le variabili utilizzate e commentare sinteticamente i vari passi dello script. 
 
-%close all;
+START
+close all;
 %clear all;
 clc;
-prompt = ('Inserisci nome file :  ');
-nome_file = input (prompt, 's');
-load (nome_file);
-%load zerointerleaving ; %si può trovare un modo per caricare direttamente in una variabile?
-y = x; %crea un secondo vettore per non modificare sequenza originale
-dim = length(y);
-prompt = 'Inserisci M:  ';
-M = input(prompt);
+
+%prompt = ('Inserisci nome file :  ');
+%nome_file = input (prompt, 's');
+%load (nome_file);
+%si può trovare un modo per caricare direttamente in una variabile?
+load zerointerleaving.mat
+%y = x; %crea un secondo vettore per non modificare sequenza originale
+dim = length(x);
+M = input('Inserisci M: ');
 f_s = 1/M;
-for i=1:M:dim
-        for j = 1:M-1
-            if (i+j)>dim
-                break
-            end
-            y(i+j) = 0; 
-        end
+%frequenza di campionamento
+
+CAMPIONAMENTO
+y_n=zeros(M,dim); %crea una matrice di zeri contenente le M sequenze lungo le righe
+Yf_n=y_n; %copia la matrice creata in quella che sarà la trasformata
+for j=1:M %ciclo per creare sequenze campionate
+    i=j;
+    while i<dim+1
+        y_n(j,i)=x(i); 
+        i=i+(M);
+    end
 end
+
+TRASFORMAZIONI
+n = (0:dim-1);
+To = dim*M;
+DF = 1/To;
+Xf=fft(x); 
 figure
-subplot (5,1,1)
-stem (x);
-subplot (5,1,2)
-stem (y);
-f  = -f_s : 1 : f_s;
-%f  = linspace(-M,M);
-filtro = M*rectangularPulse(-M/2,M/2,f);
+subplot(2,1,1)
+stem (n,x);
+xlabel ('Campioni in frequenza')
+title('Sequenza di Partenza')
+pause
+subplot(2,1,2)
+stem (DF*n,Xf,'filled');
+xlabel ('Frequenza (Hz)')
+title('Trasf seq X')
+pause
+for k=1:M
+    Yf_n(k,:)=fft(y_n(k,:));
+    figure
+    subplot(2,1,1)
+    stem(n,y_n(k,:));
+    xlabel ('Campioni in frequenza')
+    title('Sequenza campionata')
+    subplot(2,1,2)
+    stem(DF*n,Yf_n(k,:));
+    xlabel ('Frequenza (Hz)')
+    title('Sequenza trasformata')
+    pause
+end
+
+FILTRO
+%f  = -f_s : 1 : f_s;
+figure
+f  = linspace(-M,M);
+filtro = M*rectangularPulse(-M/2,M/2,f*M);
 % syms fx
 % fplot(rectangularPulse(fx), [-M/2 M/2])
-% filtro = M*rectangularPulse(-M/2,M/2,f*M);
-filtro_t = ifft (filtro);
-subplot (5,1,3)
-stem(f,filtro);
-subplot (5,1,4)
-stem(f,filtro_t);
-z = conv(y,filtro_t)
+filtro_t = fft (filtro);
+subplot (3,1,1)
+stem(f,(filtro));
+subplot (3,1,2)
+stem(f,real(filtro_t));
+%subplot (3,1,3)
+for i=1:M
+    figure
+    z = conv(y_n(i,:),filtro_t,'valid');
+    stem(z);
+end
+%% 
 
 %creare filtro rettangolo, tenerlo discreto, fare trasformata discreta. 
